2186b2fac2e3d7081cd9365dba07c54c956e2fab
Andrei Grosuliac <agrosuliac@gmail.com>
2023-04-11 18:56:38 -0400

    initial commit

---
+++ iris1.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn import tree
+from sklearn import preprocessing
+from sklearn.datasets import load_iris
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import graphviz
+
+# load the Iris dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+# print((iris.target_names[1]))
+# print((iris.target[1]))
+# create and print the decision tree
+dtc = tree.DecisionTreeClassifier(criterion="entropy")
+dtc.fit(X, y)
+tree.plot_tree(dtc)
+dot_data = tree.export_graphviz(dtc, out_file=None,
+feature_names=iris.feature_names,
+class_names=iris.target_names,
+filled=True, rounded=True,
+special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("iristree")
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+y_pred = dtc.predict(X_test)
+print(classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+val = input("Enter your value: ")
+print(val)
---
+++ iris2.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import precision_score, recall_score
+import matplotlib.pyplot as plt
+
+
+digits = load_iris() # features matrix
+# n_samples = len(digits.images)
+# data = digits.images.reshape((n_samples, -1))
+data = digits.data 
+
+X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.3, shuffle=False)
+
+
+mlp = MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000, alpha=1e-4,
+solver='sgd', verbose=10, random_state=1,
+learning_rate_init=0.001)
+mlp.fit(X_train, y_train)
+
+
+y_pred = mlp.predict(X_test)
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+pre_macro = precision_score(y_test, y_pred, average='macro')
+pre_micro = precision_score(y_test, y_pred, average='micro')
+recall_macro = recall_score(y_test, y_pred, average='macro')
+recall_micro = recall_score(y_test, y_pred, average='micro')
+
+
+cm = confusion_matrix(y_test, y_pred)
+ConfusionMatrixDisplay(cm, display_labels=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).plot()
+plt.show()
